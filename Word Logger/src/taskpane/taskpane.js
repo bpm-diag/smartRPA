d76a78d32a9a053bb5674a1df03f1297c92100aa
marco2012 <marco.fox.marco@gmail.com>
2020-01-27 13:25:18 +0100

    system logger now detects app launches

---
+++ Word Logger/src/taskpane/taskpane.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, Word */
+
+Office.onReady(info => {
+  if (info.host === Office.HostType.Word) {
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+    if (!Office.context.requirements.isSetSupported('WordApi', '1.3')) {
+      console.log('Sorry. The tutorial add-in uses Word.js APIs that are not available in your version of Office.');
+    }
+
+    // Assign event handlers and other initialization logic.
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+    document.getElementById("insert-paragraph").onclick = insertParagraph;
+
+  }
+});
+
+export async function run() {
+  return Word.run(async context => {
+    /**
+     * Insert your Word code here
+     */
+
+    // insert a paragraph at the end of the document.
+    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+    // change the paragraph color to blue.
+    paragraph.font.color = "blue";
+
+    await context.sync();
+  });
+}
+
+function insertParagraph() {
+  Word.run(function (context) {
+
+    var docBody = context.document.body;
+    docBody.onclick
+    docBody.insertParagraph("Office has several versions, including Office 2016, Office 365 Click-to-Run, and Office on the web.",
+                            "Start");
+      return context.sync();
+  })
+  .catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+          console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+  });
+}
--- logger.py
+++ logger.py
@@ -4,6 +4,8 @@ from time import sleep
 from datetime import datetime
 from getpass import getuser #user id
 from os.path import expanduser
+from sys import platform
+from sys import exit
 import csv
 from watchdog.observers import Observer
 from watchdog.events import RegexMatchingEventHandler
@@ -11,7 +13,6 @@ import win32com.client
 
 # https://pythonhosted.org/watchdog/api.html#event-handler-classes
 class MyHandler(RegexMatchingEventHandler):
-
     def __init__(self):
       super(MyHandler, self).__init__(ignore_regexes=['^[.]{1}.*', '.*/[.]{1}.*', '.*\\[.]{1}.*']) #ignore hidden files
     # def on_modified(self, event):
@@ -23,32 +24,96 @@ class MyHandler(RegexMatchingEventHandler):
     # def on_moved(self, event):
     #     pass
     def on_any_event(self, event):
-        if any(s in event.src_path for s in ['AppData','.pylint', '.ini', '.DS_Store']): #exclude folders
+        if any(s in event.src_path for s in ['AppData','.pylint', '.ini', '.DS_Store', 'node_modules']): #exclude folders
             return
         else:
             if event.event_type == "moved":
                 print(f"{datetime.now()} {getuser()} OS-System {event.event_type} {event.src_path} {event.dest_path}")
                 return
-            print(f"{datetime.now()} {getuser()} OS-System {event.event_type} {event.src_path}")
+            elif event.event_type == "modified":
+                return
+            else:
+                print(f"{datetime.now()} {getuser()} OS-System {event.event_type} {event.src_path}")
 
-def monitorProcessesWin():
-    pass
+def logProcessesWin(): 
+    #https://stackoverflow.com/a/1187338
+    strComputer = "."
+    objWMIService = win32com.client.Dispatch("WbemScripting.SWbemLocator")
+    objSWbemServices = objWMIService.ConnectServer(strComputer,"root\cimv2")
+
+    programs_to_ignore = ["sppsvc.exe", "WMIC.exe", "git.exe" ,"BackgroundTransferHost.exe", "backgroundTaskHost.exe", "MusNotification.exe", "usocoreworker.exe", "GoogleUpdate.exe"]
+
+    # create initial set of running processes
+    colItems = objSWbemServices.ExecQuery("Select * from Win32_Process")
+    running=[] #set of running programs
+    for objItem in colItems:
+        if objItem.Name not in running and objItem.Name not in programs_to_ignore:
+            running.append(objItem.Name)
+    
+    new_programs = set() #needed later to initialize 'closed' set
+    new_programs_len = 0 #needed later to check if 'new_programs' set changes
+    open_programs = [] #maitain set of open programs so I don't have duplicates when logging events
+    while True:
+        sleep(1) #seconds
+        
+        started = [] #set of programs started after the script is executed
+        colItems = objSWbemServices.ExecQuery("Select * from Win32_Process")
+        for objItem in colItems:
+            if objItem.Name not in started and objItem.Name not in programs_to_ignore: 
+                started.append(objItem.Name) 
+        
+        closed = new_programs
+        
+        new_programs = set(started) - set(running) # check the difference between the new set and the original to find new processes
+        closed_programs = closed - new_programs #find programs that are not in new_programs set anymore so they have been closed
+
+        if len(new_programs) != new_programs_len: #set is changed
+            for app in new_programs:
+                if app not in open_programs: 
+                    open_programs.append(app)
+                    pathList = list(filter(lambda prog: prog.Name == app, colItems)) #find the given program in the list of running processes and take its path
+                    if pathList: path=pathList[0].ExecutablePath # check if path exists
+                    print(f"{datetime.now()} {getuser()} AppOpen {app} {path}")
+            
+            new_programs_len = len(new_programs)
+        if len(closed_programs): #set is not empty
+            for app in closed_programs:
+                if app in open_programs: 
+                    open_programs.remove(app)
+                    pathList = list(filter(lambda prog: prog.Name == app, colItems)) #find the given program in the list of running processes and take its path
+                    if pathList: path=pathList[0].ExecutablePath # check if path exists
+                    print(f"{datetime.now()} {getuser()} AppClose {app} {path}")
+
 
 def monitorProcessesUnix():
-    pass
+    while True:
+        sleep(1)
 
 if __name__ == "__main__":
+    
     my_event_handler = MyHandler()
     path = expanduser("~") #user home folder
     my_observer = Observer()
     my_observer.schedule(my_event_handler, path, recursive=True)
     my_observer.start()
+    
     print("Logger started...")
+    
     try:
-        while True:
-            sleep(1)
+        # while True:
+        #     sleep(1)
+        if platform == "linux" or platform == "linux2": # linux
+            monitorProcessesUnix()
+        elif platform == "darwin": # OS X
+            monitorProcessesUnix()
+        elif platform == "win32": #windows
+            logProcessesWin()
+    
     except KeyboardInterrupt:
         my_observer.stop()
+    
     my_observer.join()
+    
+    
 
 
