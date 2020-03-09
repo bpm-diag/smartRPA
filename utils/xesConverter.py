f374dc88ff2f765612a0ba13df65468f30f25171
marco <marco.fox.marco@gmail.com>
2020-03-09 22:56:00 +0100

    fix xes converter

--- utils/xesConverter.py
+++ utils/xesConverter.py
@@ -1,6 +1,5 @@
 # ******************************
 # Convert csv to xes for process mining
-# http://www.xes-standard.org/downloads/doc/org/deckfour/xes/factory/XFactory.html
 # ******************************
 from threading import Thread
 
@@ -9,13 +8,13 @@ from datetime import datetime
 from opyenxes.factory.XFactory import XFactory
 from opyenxes.data_out.XesXmlSerializer import XesXmlSerializer
 from xml.etree import ElementTree
-from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import Element, Comment
 
 
 class CSV2XES:
     """This class convert csv to xes
 
-    :param csv_file_path: List of csv files to convert (list of strings representing the path of each csv). Each csv generates a trace in xes file
+    :param csv_filepath: List of csv files to convert (list of strings representing the path of each csv). Each csv generates a trace in xes file
     :param xes_filepath: Path of resulting xes file
     :param events_header: Name of the column in csv containing events
     :param timestamp_header: Name of the column in csv containing timestamps
@@ -25,23 +24,37 @@ class CSV2XES:
     :rtype: bool
     """
 
-    def __init__(self, csv_filepath: list, xes_filepath: str,
-                 events_header="event_type", timestamp_header="timestamp", timestamp_format="%Y-%m-%d %H:%M:%S:%f",
-                 attributes_to_consider=[]):
+    def __init__(self, csv_filepath: list, xes_filepath: str = None,
+                 events_header="concept:name",
+                 timestamp_header="time:timestamp",
+                 timestamp_format="%Y-%m-%d %H:%M:%S:%f",
+                 resource_header="org:resource",
+                 attributes_to_consider: list = None):
 
         self.csv_filepath = csv_filepath
-        self.xes_filepath = xes_filepath
+
+        if xes_filepath:
+            self.xes_filepath = xes_filepath
+        else:
+            self.xes_filepath = csv_filepath[-1].replace('.csv', '.xes')
+
         self.timestamp_header = timestamp_header
         self.timestamp_format = timestamp_format
         self.events_header = events_header
+        self.resource_header = resource_header
 
+        if attributes_to_consider is None:
+            attributes_to_consider = []
         self.attributes_to_consider = attributes_to_consider
         # if self.attributes_to_consider is not empty, add timestamp and event fields because they are required
         # if self.attributes_to_consider is empty it means I should consider all events
-        if self.attributes_to_consider and self.timestamp_header not in self.attributes_to_consider:
-            self.attributes_to_consider.append(self.timestamp_header)
-        if self.attributes_to_consider and self.events_header not in self.attributes_to_consider:
-            self.attributes_to_consider.append(self.events_header)
+        if self.attributes_to_consider:
+            if self.timestamp_header not in self.attributes_to_consider:
+                self.attributes_to_consider.append(self.timestamp_header)
+            if self.events_header not in self.attributes_to_consider:
+                self.attributes_to_consider.append(self.events_header)
+            if self.resource_header not in self.attributes_to_consider:
+                self.attributes_to_consider.append(self.resource_header)
 
     def run(self):
         t0 = Thread(target=self.csvToXes)
@@ -63,14 +76,17 @@ class CSV2XES:
             # handle timestamp
             if column_header == self.timestamp_header:
                 try:
-                    attribute = XFactory.create_attribute_timestamp("time:timestamp",
-                                                                    datetime.strptime(value, self.timestamp_format))
-                except ValueError:
+                    ts = datetime.strptime(value, self.timestamp_format)
+                    attribute = XFactory.create_attribute_timestamp("time:timestamp", ts)
+                except (ValueError, TypeError):
                     continue
 
             # handle event
             elif column_header == self.events_header:
                 attribute = XFactory.create_attribute_literal("concept:name", value)
+
+            elif column_header == self.resource_header:
+                attribute = XFactory.create_attribute_literal("org:resource", value)
 
             # if attributes_to_consider is empty I take all the attributes from csv, else only those in the list
             elif (self.attributes_to_consider == []) or (column_header in self.attributes_to_consider):
@@ -85,51 +101,42 @@ class CSV2XES:
     # create xml log, inserting a trace for each csv file
     def csvToXes(self):
 
+        # single log file
         log = XFactory.create_log()
-        # timestamp_list_total = list()
 
         for i, csv_path in enumerate(self.csv_filepath):
 
-            # load csv in pandas dataframe and replace nan with None
-            # https://www.w3resource.com/pandas/series/series-fillna.php
+            # load csv in pandas dataframe, replace column names to match xes standard and replace null with empty string
             try:
                 df = pandas.read_csv(csv_path, encoding='utf-8-sig') \
-                    .rename(columns={'event_type': 'concept:name',
-                                     'timestamp': 'time:timestamp',
-                                     'user': 'org:resource'}) \
-                    .fillna('')
+                    .rename(columns={self.events_header: 'concept:name',
+                                     self.timestamp_header: 'time:timestamp',
+                                     self.resource_header: 'org:resource'}) \
+                    .fillna('')  # https://www.w3resource.com/pandas/series/series-fillna.php
             except UnicodeDecodeError as e:
                 print(f"[CSV2XES] Could not decode {csv_path}: {e}")
                 return False
 
-            df.insert(0, 'case:concept:name', i)
+            try:  # insert this column to create a unique trace for each csv
+                df.insert(0, 'case:concept:name', i)
+            except ValueError:  # column already present
+                pass
 
             # create dictionary with column name as key and row data as value, like
             # {'category': 'Browser', 'event_type': 'newTab'}
-
             # if attributes_to_consider is not empty I need to take only certain columns from csv
             if self.attributes_to_consider:
                 dictionary = df[self.attributes_to_consider].to_dict(orient='records')
-            # else take all columns
-            else:
+            else:  # take all columns
                 dictionary = df.to_dict(orient='records')
 
-            # timestamp_list = list()
             events = list()
-            for row in dictionary:
-                timestamp = row['timestamp']
-                if self.__isTimestamp(timestamp):
-                    # timestamp_list.append(timestamp)
-                    event = self.__convert_line_in_event(row)
-                    events.append(event)
-                else:
-                    print(f"[CSV2XES] Can't decode timestamp {timestamp} with format {self.timestamp_format}")
-                    continue
+            [events.append(self.__convert_line_in_event(row)) for row in dictionary]
 
             trace = XFactory.create_trace()
             [trace.append(e) for e in events]
+
             log.append(trace)
-            # timestamp_list_total.extend(timestamp_list)
 
         # Save log in xes format
         with open(self.xes_filepath, "w") as file:
@@ -175,24 +182,23 @@ class CSV2XES:
                 'string', {
                     'key': 'concept:name',
                     'value': 'XES Event Log'
-                })
+                }),
+            Comment('Generated using CSV2XES library by https://github.com/marco2012/')
         ]
         tree = ElementTree.parse(self.xes_filepath)
         root = tree.getroot()
 
-        for ext in extensions:
-            root.insert(0, ext)
+        [root.insert(0, ext) for ext in extensions]
 
-        combined_csv = pandas.concat([pandas.read_csv(f, encoding="latin") for f in self.csv_filepath])
+        combined_csv = pandas.concat([pandas.read_csv(f, encoding="utf-8-sig") for f in self.csv_filepath])
         timestamp_list = combined_csv[self.timestamp_header].tolist()
-        # timestamp_list = read_csv(csv_path, usecols=[self.timestamp_header])[self.timestamp_header].tolist()
         for i, trace in enumerate(root.findall("trace")):
             try:
                 trace.insert(0,
-                     Element('string', {
-                        'key': 'concept:name',
-                        'value': timestamp_list[i]
-                    }))
+                             Element('string', {
+                                 'key': 'concept:name',
+                                 'value': timestamp_list[i]
+                             }))
             except IndexError:
                 continue
 
